f5961d391c217db4e5b22d10cac532c22b742de7
Khaled ElGabri <iKhaledElGabry@gmail.com>
2025-05-14 21:37:10 +0300

    remove get all movies and make the functionalty of it in home

--- routes/movie_routes.py
+++ routes/movie_routes.py
@@ -1,4 +1,4 @@
 from flask import Blueprint, jsonify
-from data_service import load_data
+# from data_service import load_movies
 
-movie_bp = Blueprint('movie', __name__, url_prefix='/movies')
+movie_bp = Blueprint('movie_routes', __name__, url_prefix='/movies')
