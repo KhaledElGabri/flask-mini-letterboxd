0e7efeac1faa0d92005e541520a169022f7f1eaf
Khaled ElGabri <iKhaledElGabry@gmail.com>
2025-05-16 00:55:25 +0300

    modify User class, adding generate new id function to replace the username as id

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ class User:
     User Details
     """
 
-    def __init__(self, username, password, user_id=None, profile_picture_url="", joined_on=None, movie_watched=None):
+    def __init__(self, username, password, profile_picture_url, user_id=None, joined_on=None, movie_watched=None):
         self.user_id = user_id
         self.username = username
         self.password = password
@@ -20,12 +20,6 @@ class User:
     def _hash_password(self, password):
         pass
 
-
-    # get user profile link
-    def get_profile_link(self):
-        return self.username
-
-
     # mark movie as watched
     def marked_movies(self, movie_id, movie_title):
         self.movie_watched[movie_id] = movie_title
@@ -36,11 +30,23 @@ class User:
         if movie_id in self.movie_watched:
             del self.movie_watched[movie_id]
 
+    # generate a new id
+    def generate_new_id(exist_user):
+        max_user_id = 0
+        for user in exist_user:
+            if user.user_id and user.user_id.isdigit():
+                user_id_int = int(user.user_id)
+                if user_id_int > max_user_id:
+                    max_user_id = user_id_int
+
+        return str(max_user_id + 1)
+
+
 
     # convert user object to dict (Serialization)
     def to_dict(self):
         return {
-            'user_id': self.username, # saves the username as id temporary
+            'user_id': self.user_id,
             'username': self.username,
             'password': self.password,
             'profile_picture_url': self.profile_picture_url,
@@ -52,10 +58,6 @@ class User:
     # create a user object from dict data (Deserialization)
     @classmethod
     def from_dict(cls, data):
-        if 'username' not in data:
-            print("username key error")
-            return None
-
         joined_str = data.get('joined_on')
         joined_date = datetime.date.fromisoformat(joined_str) if joined_str else None
         return cls(
